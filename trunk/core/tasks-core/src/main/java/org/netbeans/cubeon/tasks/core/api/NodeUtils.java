912d15f570249e2d3c710ff2fc40720b9208e1da
theanuradha <theanuradha@3cc9dc7e-6348-0410-b34c-715384e40188>
2008-08-11 04:23:39 +0000

    After fresh install, opening Task Explorer throws an exception

--- trunk/core/tasks-core/src/main/java/org/netbeans/cubeon/tasks/core/api/NodeUtils.java
+++ trunk/core/tasks-core/src/main/java/org/netbeans/cubeon/tasks/core/api/NodeUtils.java
@@ -91,6 +91,6 @@ public class NodeUtils {
     private static final String OPENED_ICON_KEY_UIMANAGER = "Tree.openIcon"; // NOI18N
     private static final String ICON_KEY_UIMANAGER_NB = "Nb.Explorer.Folder.icon"; // NOI18N
     private static final String OPENED_ICON_KEY_UIMANAGER_NB = "Nb.Explorer.Folder.openedIcon"; // NOI18N
-    private static final String ICON_PATH = "org/netbeans/cubeon/context/defaultFolder.gif"; // NOI18N
-    private static final String OPENED_ICON_PATH = "org/netbeans/cubeon/context/defaultFolderOpen.gif"; // NOI18N
+    private static final String ICON_PATH = "org/netbeans/cubeon/tasks/core/defaultFolder.gif"; // NOI18N
+    private static final String OPENED_ICON_PATH = "org/netbeans/cubeon/tasks/core/defaultFolderOpen.gif"; // NOI18N
 }
