16ba6cade871dae279380e80a60a2f9c93a0a93b
Anuradha G <theanuradha@users.noreply.github.com>
2008-10-20 04:53:58 +0000

    trac connector updates with query basic support

---
+++ connectors/trac/core/src/main/java/org/netbeans/cubeon/trac/query/QueryExtension.java
@@ -0,0 +1,95 @@
+/*
+ *  Copyright 2008 Anuradha.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.netbeans.cubeon.trac.query;
+
+import java.util.Collection;
+import org.netbeans.cubeon.tasks.spi.Extension;
+import org.netbeans.cubeon.tasks.spi.query.TaskQueryEventAdapter;
+import org.netbeans.cubeon.tasks.spi.task.TaskElement;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
+
+/**
+ *
+ * @author Anuradha
+ */
+public class QueryExtension implements Extension {
+
+    private InstanceContent content;
+    private Lookup lookup;
+    private AbstractTracQuery query;
+
+    public QueryExtension(AbstractTracQuery query) {
+        this.query = query;
+        content = new InstanceContent();
+        lookup = new AbstractLookup(content);
+    }
+
+    public final void remove(Object inst) {
+        content.remove(inst);
+    }
+
+    public final void add(Object inst) {
+        content.add(inst);
+    }
+
+    public Lookup getLookup() {
+        return lookup;
+    }
+
+    //events---------------------------
+    void fireAttributesUpdated() {
+        Collection<? extends TaskQueryEventAdapter> adapters = lookup.lookupAll(TaskQueryEventAdapter.class);
+        for (TaskQueryEventAdapter adapter : adapters) {
+            adapter.atributesupdated();
+        }
+
+    }
+
+    void fireSynchronizing() {
+        Collection<? extends TaskQueryEventAdapter> adapters = lookup.lookupAll(TaskQueryEventAdapter.class);
+        for (TaskQueryEventAdapter adapter : adapters) {
+            adapter.querySynchronizing();
+        }
+
+    }
+
+    void fireTaskAdded(TaskElement element) {
+        Collection<? extends TaskQueryEventAdapter> adapters = lookup.lookupAll(TaskQueryEventAdapter.class);
+        for (TaskQueryEventAdapter adapter : adapters) {
+            adapter.taskAdded(element);
+        }
+
+    }
+
+    void fireSynchronized() {
+        Collection<? extends TaskQueryEventAdapter> adapters = lookup.lookupAll(TaskQueryEventAdapter.class);
+        for (TaskQueryEventAdapter adapter : adapters) {
+            adapter.querySynchronized();
+        }
+
+    }
+
+    void fireRemoved() {
+        Collection<? extends TaskQueryEventAdapter> adapters = lookup.lookupAll(TaskQueryEventAdapter.class);
+        for (TaskQueryEventAdapter adapter : adapters) {
+            adapter.removed();
+        }
+
+    }
+}
---
+++ connectors/trac/core/src/main/java/org/netbeans/cubeon/trac/query/TracQuerySupport.java
@@ -0,0 +1,164 @@
+/*
+ *  Copyright 2008 Anuradha.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.netbeans.cubeon.trac.query;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.netbeans.cubeon.tasks.spi.query.TaskQuery;
+import org.netbeans.cubeon.tasks.spi.query.TaskQuerySupportProvider;
+import org.netbeans.cubeon.trac.repository.TracRepositoryExtension;
+import org.netbeans.cubeon.trac.repository.TracTaskRepository;
+
+/**
+ *
+ * @author Anuradha
+ */
+public class TracQuerySupport implements TaskQuerySupportProvider {
+
+    private List<TaskQuery> taskQuerys = new ArrayList<TaskQuery>(0);
+    private TracTaskRepository repository;
+    private TracRepositoryExtension extension;
+    private PersistenceHandler handler;
+    private final TracOGChangesQuery outgoingQuery;
+
+    public TracQuerySupport(TracTaskRepository repository, TracRepositoryExtension extension) {
+        this.repository = repository;
+        this.extension = extension;
+        outgoingQuery = new TracOGChangesQuery(repository);
+        handler = new PersistenceHandler(this, repository.getBaseDir());
+        handler.refresh();
+
+    }
+
+    public AbstractTracQuery createTaskQuery(AbstractTracQuery.Type type) {
+        switch (type) {
+            case FILTER:
+                return new TracFilterQuery(repository, handler.nextTaskId());
+        }
+
+        return null;
+
+    }
+
+    public void refresh() {
+        handler.refresh();
+
+    }
+
+    public List<TaskQuery> getTaskQuerys() {
+        ArrayList<TaskQuery> arrayList = new ArrayList<TaskQuery>(taskQuerys);
+        arrayList.add(0, outgoingQuery);
+        return arrayList;
+    }
+
+    public ConfigurationHandler createConfigurationHandler(TaskQuery query) {
+        ConfigurationHandler configurationHandler = null;
+        if (query != null) {
+            AbstractTracQuery abstractJiraQuery = query.getLookup().lookup(AbstractTracQuery.class);
+            switch (abstractJiraQuery.getType()) {
+                case FILTER:
+                     {
+                        //TracFilterQueryEditor editor = new JiraFilterQueryEditor(this);
+                        //editor.setQuery(query.getLookup().lookup(JiraFilterQuery.class));
+                        //configurationHandler = editor;
+                    }
+                    break;
+            }
+
+        } else {
+            //configurationHandler = new JiraFilterQueryEditor(this);
+        }
+        return configurationHandler;
+    }
+
+    public TracTaskRepository getTaskRepository() {
+        return repository;
+    }
+
+    
+
+    void setTaskQuery(List<AbstractTracQuery> localQuerys) {
+        taskQuerys = new ArrayList<TaskQuery>(localQuerys);
+    }
+
+    public void addTaskQuery(TaskQuery query) {
+
+        AbstractTracQuery localQuery = query.getLookup().lookup(AbstractTracQuery.class);
+        if (localQuery.getType() == AbstractTracQuery.Type.UTIL) {
+            return;
+        }
+        handler.addTaskQuery(localQuery);
+        taskQuerys.add(query);
+
+        extension.fireQueryAdded(query);
+    }
+
+    public void modifyTaskQuery(TaskQuery query) {
+        AbstractTracQuery localQuery = query.getLookup().lookup(AbstractTracQuery.class);
+        if (localQuery.getType() == AbstractTracQuery.Type.UTIL) {
+            return;
+        }
+        handler.addTaskQuery(localQuery);
+        localQuery.getJiraExtension().fireAttributesUpdated();
+    }
+
+    public void removeTaskQuery(TaskQuery query) {
+        AbstractTracQuery localQuery = query.getLookup().lookup(AbstractTracQuery.class);
+        if (localQuery.getType() == AbstractTracQuery.Type.UTIL) {
+            return;
+        }
+        handler.removeTaskQuery(localQuery);
+        taskQuerys.remove(localQuery);
+        extension.fireQueryRemoved(localQuery);
+        localQuery.getJiraExtension().fireRemoved();
+    }
+
+    public TaskQuery findTaskQueryById(String id) {
+        for (TaskQuery query : getTaskQuerys()) {
+            if (id.equals(query.getId())) {
+                return query;
+            }
+        }
+        return null;
+    }
+
+    public boolean canModify(TaskQuery query) {
+        AbstractTracQuery localQuery = query.getLookup().lookup(AbstractTracQuery.class);
+        switch (localQuery.getType()) {
+            case FILTER:
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
+    public boolean canRemove(TaskQuery query) {
+        AbstractTracQuery localQuery = query.getLookup().lookup(AbstractTracQuery.class);
+        switch (localQuery.getType()) {
+            case FILTER:
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
+    public TracOGChangesQuery getOutgoingQuery() {
+        return outgoingQuery;
+    }
+}
