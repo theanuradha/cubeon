3b46ff132aced3d2dea72f359b6433c5b1097ade
radoslaw.holewa <radoslaw.holewa@3cc9dc7e-6348-0410-b34c-715384e40188>
2008-12-29 17:16:34 +0000

    Bugzilla connector implementation - comments added

--- connectors/bugzilla/core/src/main/java/org/netbeans/cubeon/bugzilla/core/repository/BugzillaRepositoryAttributes.java
+++ connectors/bugzilla/core/src/main/java/org/netbeans/cubeon/bugzilla/core/repository/BugzillaRepositoryAttributes.java
@@ -25,37 +25,117 @@ import java.util.List;
  * @author radoslaw.holewa
  */
 public interface BugzillaRepositoryAttributes {
-    
+
+    /**
+     * Returns list of all available statuses.
+     *
+     * @return - list of statuses
+     */
     public List<String> getStatuses();
 
+    /**
+     * Returns list of all available statuses for closed bug.
+     *
+     * @return - list of statuses
+     */
     public List<String> getClosedStatuses();
 
+    /**
+     * Returns list of all available statuses for opened bug.
+     *
+     * @return - list of statuses
+     */
     public List<String> getOpenStatuses();
 
+    /**
+     * Returns list of all available resolutions.
+     *
+     * @return - list of resolutions
+     */
     public List<String> getResolutions();
 
+    /**
+     * Returns list of all available keywords.
+     *
+     * @return - list of keywords
+     */
     public List<String> getKeywords();
 
+    /**
+     * Returns list of all available platforms.
+     *
+     * @return - list of platforms
+     */
     public List<String> getPlatforms();
 
+    /**
+     * Returns list of all available operating systems.
+     *
+     * @return - list of operating systems
+     */
     public List<String> getOperatingSystems();
 
+    /**
+     * Returns list of all available priorities.
+     *
+     * @return - list of priorities
+     */
     public List<String> getPriorities();
 
+    /**
+     * Returns list of all available severities.
+     *
+     * @return - list of severities
+     */
     public List<String> getSeverities();
 
+    /**
+     * Returns list of all available products.
+     *
+     * @return - list of products
+     */
     public List<String> getProducts();
 
+    /**
+     * Returns default status for new bug.
+     *
+     * @return - default status
+     */
     public String getDefaultStatus();
 
+    /**
+     * Returns default severity for new bug.
+     *
+     * @return - default severity
+     */
     public String getDefaultSeverity();
 
+    /**
+     * Returns default resolution for new bug.
+     *
+     * @return - default resoulution
+     */
     public String getDefaultResolution();
 
+    /**
+     * Returns default priority for new bug.
+     *
+     * @return - default priority
+     */
     public String getDefaultPriority();
 
+    /**
+     * Returns default product for new bug.
+     *
+     * @return - default product
+     */
     public String getDefaultProduct();
 
-    public String getDefaultOperationgSystem();
+    /**
+     * Returns default operating system for new bug.
+     *
+     * @return - default operating system
+     */
+    public String getDefaultOperatingSystem();
 
 }
