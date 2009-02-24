6e122ec62d08cf58b5885a0d47138e91025e8336
theanuradha <theanuradha@3cc9dc7e-6348-0410-b34c-715384e40188>
2009-02-24 07:09:08 +0000

    Issue 70: Attache debugger breakpoints to current task
    breakpoint support added

---
+++ trunk/extensions/debugger-bridge/src/main/java/org/netbeans/cubeon/debugger/bridge/Installer.java
@@ -0,0 +1,73 @@
+/*
+ *  Copyright 2009 Anuradha.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.netbeans.cubeon.debugger.bridge;
+
+import org.netbeans.api.debugger.Breakpoint;
+import org.netbeans.api.debugger.DebuggerManager;
+import org.netbeans.api.debugger.DebuggerManagerAdapter;
+import org.netbeans.cubeon.tasks.core.api.CubeonContext;
+import org.netbeans.cubeon.tasks.core.api.CubeonContextListener;
+import org.netbeans.cubeon.tasks.spi.task.TaskElement;
+import org.openide.modules.ModuleInstall;
+import org.openide.util.Lookup;
+
+/**
+ * Manages a module's lifecycle. Remember that an installer is optional and
+ * often not needed at all.
+ */
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        final DebuggerManager debuggerManager = DebuggerManager.getDebuggerManager();
+        final CubeonContext context = Lookup.getDefault().lookup(CubeonContext.class);
+        assert context != null;
+
+        context.addContextListener(new CubeonContextListener() {
+
+            public void taskActivated(TaskElement element) {
+                Breakpoint[] breakpoints = debuggerManager.getBreakpoints();
+                for (Breakpoint breakpoint : breakpoints) {
+                    if (element.getDisplayName().equals(breakpoint.getGroupName())) {
+                        breakpoint.enable();
+                    }
+                }
+            }
+
+            public void taskDeactivated(TaskElement element) {
+                Breakpoint[] breakpoints = debuggerManager.getBreakpoints();
+                for (Breakpoint breakpoint : breakpoints) {
+                    if (element.getDisplayName().equals(breakpoint.getGroupName())) {
+                        breakpoint.disable();
+                    }
+                }
+            }
+        });
+
+        debuggerManager.addDebuggerListener(new DebuggerManagerAdapter() {
+
+            @Override
+            public void breakpointAdded(Breakpoint breakpoint) {
+                if (context.getActive() != null) {
+                    breakpoint.setGroupName(context.getActive().getDisplayName());
+                }
+            }
+        });
+
+
+    }
+}
