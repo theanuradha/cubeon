3e5b4ca2801770e51fa6ba9892db7d0fd835d64e
theanuradha <theanuradha@3cc9dc7e-6348-0410-b34c-715384e40188>
2009-10-01 04:34:12 +0000

    task contexts moved to repository level

--- trunk/context/context-api/src/main/java/org/netbeans/cubeon/context/internal/ContextManagerImpl.java
+++ trunk/context/context-api/src/main/java/org/netbeans/cubeon/context/internal/ContextManagerImpl.java
@@ -16,17 +16,22 @@
  */
 package org.netbeans.cubeon.context.internal;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
 import org.netbeans.cubeon.context.api.TaskContext;
 import org.netbeans.cubeon.context.api.TaskContextHandler;
 import org.netbeans.cubeon.context.api.TaskContextManager;
 import org.netbeans.cubeon.context.spi.TaskResouresProvider;
 import org.netbeans.cubeon.tasks.core.api.CubeonContext;
 import org.netbeans.cubeon.tasks.core.api.CubeonContextListener;
+import org.netbeans.cubeon.tasks.core.api.TaskRepositoryHandler;
 import org.netbeans.cubeon.tasks.core.spi.TaskNodeView;
+import org.netbeans.cubeon.tasks.spi.repository.TaskRepository;
 import org.netbeans.cubeon.tasks.spi.task.TaskElement;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
@@ -56,15 +61,57 @@ public class ContextManagerImpl implements TaskContextManager {
         }
         assert contextDir != null;
         context = Lookup.getDefault().lookup(CubeonContext.class);
+
+
+// <editor-fold defaultstate="collapsed" desc="TODO: REMOVE after version 1.2">
+        //move with old context data to repository base location
+        File toFile = FileUtil.toFile(contextDir);
+        if (toFile != null) {
+            File[] listFiles = toFile.listFiles(new FileFilter() {
+
+                public boolean accept(File pathname) {
+                    return pathname.isFile() && pathname.getName().endsWith(".xml");
+                }
+            });
+            if (listFiles.length > 0) {
+                //lookup CubeonContext
+
+                TaskRepositoryHandler repositoryHandler = context.getLookup().lookup(TaskRepositoryHandler.class);
+                List<TaskRepository> taskRepositorys = repositoryHandler.getTaskRepositorys();
+                OUTER:
+                for (File file : listFiles) {
+                    for (TaskRepository repository : taskRepositorys) {
+                        String fileName = file.getName().replace(".xml", "");
+                        TaskElement taskElement = repository.getTaskElementById(fileName);
+                        if (taskElement != null) {
+                            try {
+                                FileObject taskContextDir = FileUtil.createFolder(contextDir, repository.getId());
+                                FileObject taskFileObject = FileUtil.toFileObject(file);
+                                if (taskFileObject != null) {
+                                    FileUtil.moveFile(taskFileObject, taskContextDir, fileName);
+                                }
+                            } catch (IOException ex) {
+                                Logger.getLogger(ContextManagerImpl.class.getName()).warning(ex.getMessage());
+                            }
+                            continue OUTER;
+                        }
+                    }
+                    file.delete();
+                }
+            }
+
+        }// </editor-fold>
+
         context.addContextListener(new CubeonContextListener() {
 
-            public void taskActivated(TaskElement element) {}
+            public void taskActivated(TaskElement element) {
+            }
 
             public void taskDeactivated(TaskElement element) {
                 synchronized (LOCK) {
                     if (activeContext != null && activeContext.getTask().equals(element)) {
                         activeContext = null;
-                        activeContextHandler=null;
+                        activeContextHandler = null;
                     }
                 }
             }
@@ -99,7 +146,15 @@ public class ContextManagerImpl implements TaskContextManager {
     }
 
     public TaskContextHandler getContextHandler(TaskElement element) {
+        TaskRepository taskRepository = element.getTaskRepository();
 
+        try {
+            FileObject taskContextDir = FileUtil.createFolder(contextDir, taskRepository.getId());
+            return new ContextHandlerImpl(taskContextDir, element);
+        } catch (IOException ex) {
+            Logger.getLogger(ContextManagerImpl.class.getName()).warning(ex.getMessage());
+        }
+        //use old dir
         return new ContextHandlerImpl(contextDir, element);
     }
 
