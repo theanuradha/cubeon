f36dc377032d638fe03e83a58feb3d6db1e7d8fb
theanuradha <theanuradha@3cc9dc7e-6348-0410-b34c-715384e40188>
2009-10-21 12:51:10 +0000

    Issue 16: added google code api client serviceIssue 16: switch to json-simple and project stubs

---
+++ trunk/connectors/gcode/core/src/main/java/org/netbeans/cubeon/gcode/repository/GCodeTaskRepository.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2009 Anuradha.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.netbeans.cubeon.gcode.repository;
+
+import java.awt.Image;
+import org.netbeans.cubeon.tasks.spi.Notifier;
+import org.netbeans.cubeon.tasks.spi.repository.RepositoryEventAdapter;
+import org.netbeans.cubeon.tasks.spi.repository.TaskRepository;
+import org.netbeans.cubeon.tasks.spi.task.TaskElement;
+import org.openide.util.Lookup;
+
+/**
+ *
+ * @author Anuradha
+ */
+public class GCodeTaskRepository implements TaskRepository{
+
+    public String getId() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getDescription() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Lookup getLookup() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Image getImage() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public TaskElement createTaskElement(String summery, String description) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public TaskElement getTaskElementById(String id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void persist(TaskElement element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void synchronize() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public State getState() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Notifier<RepositoryEventAdapter> getNotifier() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
