357901542faf045a8868c5b3b2ab765d19088c43
theanuradha <theanuradha@3cc9dc7e-6348-0410-b34c-715384e40188>
2009-10-22 16:42:46 +0000

    Issue 16: gcode repository provider impl

--- trunk/connectors/gcode/core/src/main/java/org/netbeans/cubeon/gcode/repository/GCodeTaskRepository.java
+++ trunk/connectors/gcode/core/src/main/java/org/netbeans/cubeon/gcode/repository/GCodeTaskRepository.java
@@ -14,40 +14,90 @@
  *  limitations under the License.
  *  under the License.
  */
-
 package org.netbeans.cubeon.gcode.repository;
 
 import java.awt.Image;
+import java.util.List;
+import org.netbeans.cubeon.gcode.api.GCodeException;
 import org.netbeans.cubeon.tasks.spi.Notifier;
 import org.netbeans.cubeon.tasks.spi.repository.RepositoryEventAdapter;
 import org.netbeans.cubeon.tasks.spi.repository.TaskRepository;
 import org.netbeans.cubeon.tasks.spi.task.TaskElement;
+import org.openide.filesystems.FileObject;
+import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
 
 /**
  *
  * @author Anuradha
  */
-public class GCodeTaskRepository implements TaskRepository{
+public class GCodeTaskRepository implements TaskRepository {
 
+    private final String id;
+    private String name;
+    private String description;
+    private String project;
+    private String user;
+    private String password;
+    private final GCodeTaskRepositoryProvider provider;
+    private final Lookup lookup;
+    private State state = State.INACTIVE;
+    private final GCodeRepositoryExtension extension;
+    public GCodeTaskRepository(GCodeTaskRepositoryProvider provider,
+            String id, String name, String description) {
+        this.provider = provider;
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        extension = new GCodeRepositoryExtension(this);
+
+
+
+        lookup = Lookups.fixed(this,  provider,extension );
+    }
     public String getId() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return id;
     }
 
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return name;
     }
 
     public String getDescription() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return description != null ? description : name;
     }
 
     public Lookup getLookup() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return lookup;
     }
 
     public Image getImage() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return ImageUtilities.loadImage("org/netbeans/cubeon/gcode/gcode-repository.png");
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
     }
 
     public TaskElement createTaskElement(String summery, String description) {
@@ -67,11 +117,26 @@ public class GCodeTaskRepository implements TaskRepository{
     }
 
     public State getState() {
-        throw new UnsupportedOperationException("Not supported yet.");
+       return state;
     }
 
-    public Notifier<RepositoryEventAdapter> getNotifier() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public GCodeRepositoryExtension getNotifier() {
+        return extension;
     }
 
+    List<String> getTaskIds() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    FileObject getBaseDir() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void reconnect() throws GCodeException{
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void updateAttributes() throws GCodeException{
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
--- trunk/connectors/gcode/core/src/main/java/org/netbeans/cubeon/gcode/repository/GCodeTaskRepositoryProvider.java
+++ trunk/connectors/gcode/core/src/main/java/org/netbeans/cubeon/gcode/repository/GCodeTaskRepositoryProvider.java
@@ -17,12 +17,27 @@
 package org.netbeans.cubeon.gcode.repository;
 
 import java.awt.Image;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.netbeans.cubeon.gcode.api.GCodeException;
+import org.netbeans.cubeon.gcode.persistence.RepositoryInfo;
+import org.netbeans.cubeon.gcode.persistence.RepositoryPersistence;
+import org.netbeans.cubeon.gcode.utils.GCodeExceptionHandler;
+import org.netbeans.cubeon.tasks.core.api.RepositoryUtils;
+import org.netbeans.cubeon.tasks.core.api.TaskEditorFactory;
 import org.netbeans.cubeon.tasks.spi.repository.TaskRepository;
 import org.netbeans.cubeon.tasks.spi.repository.TaskRepositoryType;
+import org.netbeans.cubeon.tasks.spi.task.TaskElement;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.Repository;
+import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
 import org.openide.util.lookup.Lookups;
 
 /**
@@ -30,6 +45,23 @@ import org.openide.util.lookup.Lookups;
  * @author Anuradha
  */
 public class GCodeTaskRepositoryProvider implements TaskRepositoryType {
+
+    static final String BASE_PATH = "cubeon/gcode_repositories/";//NOI18N
+    private List<GCodeTaskRepository> taskRepositorys = new ArrayList<GCodeTaskRepository>();
+    private FileObject baseDir = null;
+    private AtomicBoolean initiailzed = new AtomicBoolean(false);
+    private final RepositoryPersistence persistence;
+
+    public GCodeTaskRepositoryProvider() {
+        try {
+            baseDir = FileUtil.createFolder(Repository.getDefault().
+                    getDefaultFileSystem().getRoot(), BASE_PATH);
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        assert baseDir != null;
+        persistence = new RepositoryPersistence(FileUtil.toFile(baseDir));
+    }
 
     public String getId() {
         return "gcode";//NOI18N
@@ -51,23 +83,115 @@ public class GCodeTaskRepositoryProvider implements TaskRepositoryType {
         return Lookups.fixed(this);
     }
 
-    public TaskRepository persistRepository(TaskRepository taskRepository) {
-        throw new UnsupportedOperationException("Not supported yet.");
+ public TaskRepository persistRepository(TaskRepository repository) {
+
+        final GCodeTaskRepository gCodeTaskRepository =
+                repository.getLookup().lookup(GCodeTaskRepository.class);
+        if (gCodeTaskRepository != null) {
+            
+            if (!taskRepositorys.contains(gCodeTaskRepository)) {
+                taskRepositorys.add(gCodeTaskRepository);
+            }
+            List<RepositoryInfo> repositoryInfos = getRepositoryInfos();
+            persistence.persistRepositoryInfos(repositoryInfos);
+            GCodeRepositoryExtension extension = gCodeTaskRepository.getNotifier();
+            extension.fireNameChenged();
+            extension.fireDescriptionChenged();
+            RequestProcessor.getDefault().post(new Runnable() {
+
+                public void run() {
+                    try {
+                        //reconnect and update repository
+                        gCodeTaskRepository.reconnect();
+                        gCodeTaskRepository.updateAttributes();
+                        gCodeTaskRepository.synchronize();
+                    } catch (GCodeException ex) {
+                        GCodeExceptionHandler.notify(ex);
+                    }
+                }
+            });
+
+            return repository;
+        }
+
+        return null;
     }
 
-    public boolean removeRepository(TaskRepository taskRepository) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private  List<RepositoryInfo> getRepositoryInfos() {
+        List<RepositoryInfo> repositoryInfos = new ArrayList<RepositoryInfo>(taskRepositorys.size());
+        for (GCodeTaskRepository gctr : taskRepositorys) {
+            repositoryInfos.add(new RepositoryInfo(gctr.getId(), gctr.getName(), gctr.getDescription(), gctr.getProject(), gctr.getUser(), RepositoryUtils.encodePassword(gctr.getUser(), gctr.getPassword())));
+        }
+        return repositoryInfos;
     }
 
-    public List<? extends TaskRepository> getRepositorys() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean removeRepository(TaskRepository repository) {
+
+        GCodeTaskRepository gcodeTaskRepository =
+                repository.getLookup().lookup(GCodeTaskRepository.class);
+        if (gcodeTaskRepository != null) {
+
+            taskRepositorys.remove(gcodeTaskRepository);
+            persistence.persistRepositoryInfos(getRepositoryInfos());
+            List<String> taskIds = gcodeTaskRepository.getTaskIds();
+            TaskEditorFactory factory = Lookup.getDefault().lookup(TaskEditorFactory.class);
+            for (String id : taskIds) {
+                TaskElement task = gcodeTaskRepository.getTaskElementById(id);
+                if (task != null) {
+                    gcodeTaskRepository.getNotifier().fireTaskRemoved(task);
+                    factory.closeTask(task);
+                }
+
+            }
+            //FIXME
+//            List<TaskQuery> querys = gcodeTaskRepository.getQuerySupport().getTaskQuerys();
+//            for (TaskQuery query : querys) {
+//                gcodeTaskRepository.getQuerySupport().removeTaskQuery(query);
+//            }
+
+            try {
+                FileObject baseDir1 = gcodeTaskRepository.getBaseDir();
+
+                baseDir1.delete();
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            return true;
+        }
+
+        return false;
     }
 
-    public TaskRepository getRepositoryById(String Id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<TaskRepository> getRepositorys() {
+        if (!initiailzed.getAndSet(true)) {
+            List<RepositoryInfo> repositoryInfos = persistence.getRepositoryInfos();
+            for (RepositoryInfo repositoryInfo : repositoryInfos) {
+                GCodeTaskRepository repository = new GCodeTaskRepository(this, repositoryInfo.getId(), repositoryInfo.getName(), repositoryInfo.getDescription());
+                repository.setUser(repositoryInfo.getUser());
+                repository.setProject(repositoryInfo.getProject());
+                repository.setPassword(RepositoryUtils.decodePassword(repositoryInfo.getUser(),
+                repository.getPassword()));
+                taskRepositorys.add(repository);
+            }
+        }
+        return new ArrayList<TaskRepository>(taskRepositorys);
+    }
+
+    public TaskRepository getRepositoryById(String id) {
+        for (TaskRepository taskRepository : taskRepositorys) {
+            if (taskRepository.getId().equals(id)) {
+                return taskRepository;
+            }
+        }
+
+        return null;
     }
 
     public ConfigurationHandler createConfigurationHandler() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return null;//new ConfigurationHandlerImpl(this);
+    }
+
+    public FileObject getBaseDir() {
+        return baseDir;
     }
 }
